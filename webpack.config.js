c255a27addf75f896b74d2100db216ac63514cff
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-25 11:32:40 -0700

    bug: added missing comma

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'babel',
       query: {
         presets: ['es2015' , 'react']
       }
