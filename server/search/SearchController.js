f3a2ecbb3f04ae1bf2ee1bfb857704b3e3736005
Alex Paczynski <apaczy@gmail.com>
2016-04-26 15:55:23 -0700

    core: GitHub repo search request function in progress

--- server/search/SearchController.js
+++ server/search/SearchController.js
@@ -1,11 +1,18 @@
 var path = require('path');
+var request = require('request');
 //res.sendFile(path.resolve('temp/index.html'));
 
 module.exports = {
+	var root = 'https://www.api.github.com/';
 	//create methods here that supply logic for SearchRouter.js
 	getRepos: function (req, res) {
 		var query = req.body.searchTerm;
-
+		request({
+			uri: root + 'search/repositories',
+			method: 'GET'
+		}, function (err, res, body) {
+			console.log('Successful request to GitHub repo search! ', body);
+		})
 	},
 
 	getCode: function (req, res) {
