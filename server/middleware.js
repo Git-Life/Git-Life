f5ccfa86d6e11e3d1b73bebe5fe0b4b0705c44e6
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-25 19:26:07 -0700

    bug: pointed pathnames to proper bundle js location

--- server/middleware.js
+++ server/middleware.js
@@ -8,8 +8,9 @@ module.exports = function(app, express) {
   app.use(bodyParser.urlencoded({ extended: true }));
 
   //app.use(express.static(path.join(__dirname, 'client/')));
-  app.use(express.static(path.join(__dirname, 'dist/')));
-  app.use('/', homeRouter);
+  app.use(express.static(path.join(__dirname, '../dist')));
+  console.log(path.join(__dirname, '../dist'));
+  //app.use('/', homeRouter);
 
   require('./home/HomeRouter.js')(homeRouter);
 };
--- server/server.js
+++ server/server.js
@@ -2,18 +2,7 @@ var express = require('express');
 
 var app = express();
 
-// If using ES6 and WebPack comment out line 10
-// app.use(webpackMiddleware(compiler, {
-//   quiet: true,
-//   noInfo: true,
-//   stats: {
-//     colors: true,
-//     reasons: true
-//   },
-//   publicPath: webpackConfig.output.publicPath
-// }));
-// app.use(webpackHot(compiler));
-// app.use(express.static(path.join(__dirname, 'es6Refactor/')));
+
 
 require('./middleware.js')(app, express);
 
