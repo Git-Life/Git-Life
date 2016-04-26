342897ab95f55593ffc915b83e0f5adb84c621e7
Alex Paczynski <apaczy@gmail.com>
2016-04-26 16:55:55 -0700

    core: GitHub /search/repositories request working with proper response

--- server/search/SearchController.js
+++ server/search/SearchController.js
@@ -2,18 +2,22 @@ var path = require('path');
 var request = require('request');
 //res.sendFile(path.resolve('temp/index.html'));
 
-var root = 'https://www.api.github.com/';
+var root = 'https://api.github.com/';
 
 module.exports = {
 	//create methods here that supply logic for SearchRouter.js
 	getRepos: function (req, res) {
 		var query = req.query.searchTerm;
-		console.log('request to server: ', req.query);
 		request({
-			uri: root + 'search/repositories',
-			method: 'GET'
+			uri: root + 'search/repositories?q=' + query,
+			method: 'GET',
+      headers: {'user-agent': 'node.js'}
+
+      
+
 		}, function (error, response, body) {
-			console.log('Successful request to GitHub repo search! ', response);
+			console.log('Successful request to GitHub repo search! ', body);
+			console.log('Error: ', error);
 		});
 	},
 
