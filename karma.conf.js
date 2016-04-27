5ce4efa28719aef77488311cd523e159163c77d4
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-27 10:37:36 -0700

    core: changed karma conf to copy enzyme example

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,67 @@
-webpack: {
-  devtool: 'inline-source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /\/node_modules\//,
-        loader: 'babel',
-        query: {
-          presets: ['airbnb']
-        }
+var path = require('path');
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'test/**/*.js'
+    ],
+
+    preprocessors: {
+      // add webpack as preprocessor
+      'src/**/*.js': ['webpack', 'sourcemap'],
+      'test/**/*.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: { //kind of a copy of your webpack config
+      devtool: 'inline-source-map', //just do inline source maps instead of the default
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: path.resolve(__dirname, 'node_modules'),
+            query: {
+              presets: ['airbnb']
+            }
+          },
+          {
+            test: /\.json$/,
+            loader: 'json',
+          },
+        ]
+      },
+      externals: {
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
       }
-    ]
-    externals: {
-      'cheerio': 'window',
-      'react/lib/ExecutionEnvironment': true,
-      'react/lib/ReactContext': true
-    }
-  }
-}
+    },
+
+    webpackServer: {
+      noInfo: true //please don't spam the console when running in karma!
+    },
+
+    plugins: [
+      'karma-webpack',
+      'karma-jasmine',
+      'karma-sourcemap-loader',
+      'karma-chrome-launcher',
+      'karma-phantomjs-launcher'
+    ],
+
+
+    babelPreprocessor: {
+      options: {
+        presets: ['airbnb']
+      }
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+  })
+};
