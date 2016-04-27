fb8d9f4a22479611e6394c85f5733126b3a04e16
Jenna Gain <gain.jenna@gmail.com>
2016-04-26 17:12:21 -0700

    core: Created Redux Store

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/searchbar';
+import SearchBar from './containers/searchbar';
 import SearchResults from'./components/searchresults';
 
 
