08785003e3b1fb203d7e30dcfae7029d66afaa46
Jenna Gain <gain.jenna@gmail.com>
2016-04-27 10:54:16 -0700

    Core: Refactored SearchBar with Redux

--- client/containers/searchbar.jsx
+++ client/containers/searchbar.jsx
@@ -38,21 +38,16 @@ export default class SearchBar extends Component {
 
   }
   handleSearch(value){
-    this.searchGit(value);
     this.setState({"searchTerm":value});
+    this.searchGit(value);
+
     console.log("state", this.state.searchTerm);
   }
   render() {
     return(
       <div >
-          <input
-          value={this.state.searchTerm}
-          onChange={(event) => {
-            this.setState({searchTerm: event.target.value }, () =>{this.searchGit(this.state.searchTerm)});
-            /*setTimeout((function() {
-              console.log(this.state);
-            }).bind(this), 100)*/
-          }}
+          <input value={this.state.searchTerm} onChange={(event) => {this.handleSearch(event.target.value)}}
+
           placeholder="Search GitHub" />
         <button onClick={()=>{this.searchGit(this.state.searchTerm); }}>Search</button>
 
