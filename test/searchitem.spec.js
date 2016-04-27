9610399371bf50fe02950d8d621b44ff8f96b805
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-27 16:44:11 -0700

    core: established baseline testing

--- test/searchitem.spec.js
+++ test/searchitem.spec.js
@@ -4,18 +4,16 @@ import TestUtils from 'react-addons-test-utils';
 import React from 'react';
 
 var component;
-var spy = sinon.spy();
 
 describe('Given an instance of the SearchItem', () => {
   describe('when we render the component', () => {
     before(() => {
-      component = TestUtils.renderIntoDocument(<SearchItem onRender={ spy } />);
+      component = TestUtils.renderIntoDocument(<SearchItem/>);
     });
     it('should render a paragraph', () => {
       var paragraph = TestUtils.scryRenderedDOMComponentsWithTag(component, 'p');
 
       expect(paragraph).to.have.length.above(0, 'Expected to have element with tag <p>');
-      expect(spy).to.be.calledOnce;
     });
   });
 });
