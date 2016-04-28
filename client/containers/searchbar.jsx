e11e71ff6ea7bdf84b666503db9c86d0e83e79a4
Jenna Gain <gain.jenna@gmail.com>
2016-04-28 16:04:40 -0700

    Core: SearchBar fixed

--- client/containers/searchbar.jsx
+++ client/containers/searchbar.jsx
@@ -1,19 +1,28 @@
-import _ from 'lodash';
+import {reduce} from 'lodash';
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props)
+    this.state = {searchTerm: ''};
+    console.log("constructor",this.props.searchTerm)
+  }
+
   handleSearch(value){
     this.props.onSearchTermChange(value);
+    this.setState({searchTerm: value});
     this.props.onRequest(value);
+
   }
+
   render() {
     return(
       <div >
           <input
             onChange={(event) => {this.handleSearch(event.target.value)}}
           placeholder="Search GitHub" />
-        <button onClick={()=>{this.handleSearch(this.state.searchTerm);}}>Search</button>
-      </div>
+        <button onClick={(event)=>{ this.handleSearch(this.state.searchTerm)}}>Search</button>
+    </div>
     );
   }
 }
