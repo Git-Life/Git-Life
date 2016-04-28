05099c497c548445686d5fee934337660c4dd94e
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-28 15:41:01 -0700

    bug: used proper chai syntax

--- test/searchitem.spec.jsx
+++ test/searchitem.spec.jsx
@@ -7,15 +7,15 @@ var component;
 
 describe('Given an instance of the SearchItem', () => {
   describe('when we render the component', () => {
-
-
-    it('should do a thing', () => {
+    before(() => {
       var renderer = TestUtils.createRenderer();
       renderer.render(<SearchItem />);
-      var subject = renderer.getRenderOutput();
+      component = renderer.getRenderOutput();
+    });
 
-    //expect(2).to.be(2);
-    expect('test').to.be.a('string');
+    it('should do a thing', () => {
+      expect(component.type).to.equal('div');
+
   });
 
   });
