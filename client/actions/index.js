36e67a9350de26af4a31683f40b7f95a276813e0
Sheryl Hohman <sherylhohman@yahoo.com>
2016-04-29 16:43:09 -0700

    core: getting github data, using public key, and alex's query string

--- client/actions/index.js
+++ client/actions/index.js
@@ -9,26 +9,9 @@ export function updateSearchTerm(searchTerm = null){
 }
 
 
-export const REQUEST_GIT = 'REQUEST_GIT';
-export function searchGitHub(searchTerm){
-  var results = Axios.get('/search/repos', {
-      params: {
-      searchTerm: searchTerm
-      }
-  });
-  return {
-    type: REQUEST_GIT,
-    payload: results
-  }
-
-}
-
-
-// // ***** TEMP *** TO TEST commitsController,js ********
-// // hide above code, to test mine using the pre-built SEARCHTERM react/redux/gitRequest
 // export const REQUEST_GIT = 'REQUEST_GIT';
 // export function searchGitHub(searchTerm){
-//   var results = Axios.get('/splash/commits', {
+//   var results = Axios.get('/search/repos', {
 //       params: {
 //       searchTerm: searchTerm
 //       }
@@ -39,3 +22,21 @@ export function searchGitHub(searchTerm){
 //   }
 
 // }
+
+
+// // ***** TEMP *** TO TEST commitsController,js ********
+// // hide above code, to test mine using the pre-built SEARCHTERM react/redux/gitRequest
+
+export const REQUEST_GIT = 'REQUEST_GIT';
+export function searchGitHub(searchTerm){
+  var results = Axios.get('/splash/commits', {
+      params: {
+      searchTerm: searchTerm
+      }
+  });
+  return {
+    type: REQUEST_GIT,
+    payload: results
+  }
+
+}
--- server/splash/commitsController.js
+++ server/splash/commitsController.js
@@ -5,41 +5,83 @@ const SECRET = require('../../config.js');
 const GITHUB_ROOT = 'https://api.github.com/';
 
 const SECRET_URL  = '&client_id=' + SECRET.clientID + '&client_secret=' + SECRET.clientSecret;
+console.log('in the commitsController file..');
 
-var   timeOfLastGitRequest;
+var   timeOfLastGitRequest = null;
 const ONE_DAY_SECONDS = 86400000;
+const DEV_TIMEOUT = 1000; // milisec
 
 module.exports = function(req, res){
+
   // return early if we've already made an API request today
-  if ( (timeOfLastGitRequest !== undefined) ||
-       (!((Date.now() - timeOfLastGitRequest) > ONE_DAY_SECONDS))) {
-    return;
-  }
+  // console.log('in commitsController');
+  // if ( (timeOfLastGitRequest !== null) &&
+  //      (((Date.now() - timeOfLastGitRequest) < DEV_TIMEOUT))) {
+  //   console.log('early return from  commitsController.js');
+  //   res.send(JSON.parse({}));
+
+  //   return;
+  // }
 
   // Query API
 
-    var gitRequest = 'search/repositories?q=size:>1000&pushed=>2016-4-25&sort=stars&order=desc'
+    // var gitRequest = 'size:>1000&pushed=>2016-4-25&sort=stars&order=desc'
+    // //var query = req.query.searchTerm;
+    // var query = 'search/repositories?q=react-redux';
+
+    // works using Alex's query string
+    var query = 'search/repositories?q=react';
+    console.log('about to create request (comitsController.js)');
+
 
     request({
-      uri:    GITHUB_ROOT + gitRequest + SECRET_URL,
+      // uri:    GITHUB_ROOT + gitRequest + query + SECRET_URL,
+      // right now just use the public API - above is my key, which got rate limited
+      uri:    GITHUB_ROOT + query,
       method: 'GET',
       headers: {'user-agent': 'node.js'}
-
       }, function (error, response, body) {
+        console.log('in function body (comitsController.js)');
+
           if(error){
             console.log('Error: ', error);
           }
 
           fs.writeFile(__dirname + '/../dummyData_SH_temp/commits.txt', body, (err) => {
             if(err){
-              console.log(err);
+              console.log('fs.writeFile error in server/splash/commitsController.js', err);
             }
 
-          console.log('file was saved');
-          lastTimeChecked = new Date();
+          console.log('fs: "/../dummyData_SH_temp/commits.txt" was saved with shQuery Results');
+          timeOfLastGitRequest = new Date();
+          console.log(timeOfLastGitRequest);
         });
+
+        console.log('res.send() executed, (server/splash/commitsController.js)');
+        res.send(JSON.parse(body).items);
+
       });
   }
+
+/**** sample from search bar github api query ***
+
+   getRepos: function (req, res) {
+    var query = req.query.searchTerm;
+    request({
+      uri: root + 'search/repositories?q=' + query,
+      method: 'GET',
+      headers: {'user-agent': 'node.js'}
+    }, function (error, response, body) {
+      if(error){
+        console.log('Error: ', error);
+      }
+      res.send(JSON.parse(body).items);
+    });
+  },
+
+*/
+
+
 
     // console.log('this is body', JSON.parse(body).items);
     //for top 10 results
