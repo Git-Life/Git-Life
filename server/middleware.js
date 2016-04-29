4ebaf94166ccc734d7edb9d1d0b83639b4de3a88
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-28 17:14:10 -0700

    core: added endpoint in middleware for splash

--- server/middleware.js
+++ server/middleware.js
@@ -3,12 +3,15 @@ var bodyParser = require('body-parser');
 
 module.exports = function(app, express) {
 	var searchRouter = express.Router();
+  var splashRouter = express.Router();
 
 	app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use(express.static(path.join(__dirname, '../dist')));
   app.use('/search', searchRouter);
+  app.use('/splash', splashRouter);
 
   require('./search/SearchRouter.js')(searchRouter);
+  require('./splash/splashRouter.js')(splashRouter);
 };
