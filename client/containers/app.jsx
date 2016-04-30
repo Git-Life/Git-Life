515e2f504185ac4d1be876a81a42897f2122c202
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-29 18:40:31 -0700

    core: added splashrepoes to index container

--- client/containers/app.jsx
+++ client/containers/app.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './searchbar';
 import SearchResults from'../components/searchresults';
+import SplashRepos from './container_splash_repos';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as Actions from '../actions';
@@ -12,6 +13,7 @@ class App extends Component {
       <div>
         <SearchBar searchTerm={this.props.term} onRequest={this.props.actions.searchGitHub} onSearchTermChange={this.props.actions.updateSearchTerm}/>
         <SearchResults results={this.props.results}/>
+        <SplashRepos />
       </div>
     );
   }
