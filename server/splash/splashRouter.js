a00d290ab9a23305d26d833a548c9a7db49790f7
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-29 19:59:42 -0700

    core: sending the mass package now from server

--- server/splash/splashRouter.js
+++ server/splash/splashRouter.js
@@ -2,9 +2,6 @@ var trendReposController = require('./trendReposController.js');
 
 
 module.exports = function (router) {
-  //router.get('/repos', trendReposController);
-  router.get('/repos', function(req, res){
-    console.log('we got a request');
-    res.send('hello');
-  })
+  router.get('/repos', trendReposController);
+
 };
