46e980fff1fdb243a7fbf48331ad17f9d353ce44
Caleb Rogers <rogersjcaleb@gmail.com>
2016-04-29 17:26:10 -0700

    core: added reference to splashrepos reducer in main reducer file

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import ReducerResults from './reducer_results';
 import ReducerTerm from './reducer_term';
+import ReducerSplashRepos from './reducer_splash_repos';
 
 const rootReducer = combineReducers({
   term: ReducerTerm,
-  results: ReducerResults
+  results: ReducerResults,
+  splashRepos: ReducerSplashRepos
 });
 
 export default rootReducer;
