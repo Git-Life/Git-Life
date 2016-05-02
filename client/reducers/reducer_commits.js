2a15631afe64b34a446e93dc8c3ba01d487e3bed
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-02 10:47:57 -0700

    syn: cleaned up console logs and comments

--- client/reducers/reducer_commits.js
+++ client/reducers/reducer_commits.js
@@ -5,9 +5,6 @@ const initialState = [];
 export default function(state = initialState, action) {
   switch(action.type){
     case COMMIT_DATA:
-      // return Object.assign({}, state, {
-      //   data: action.payload.data
-      // });
       return action.payload.data;
     default:
       return state;
