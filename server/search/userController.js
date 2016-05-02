0943d82b991adf2b74164fcf0f90d290ca8f2c83
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-02 16:00:46 -0700

    feat: server parses contributors data into array of users with #count and #contributions

--- server/search/userController.js
+++ server/search/userController.js
@@ -14,9 +14,40 @@ function gitHTTP(method, reqString, cb){
   }, cb);
 }
 
+var userObj = {};
+
 module.exports = function(body, res){
+  var items =   JSON.parse(body).items;
+  for(var i = 0; i < 10; i++){
 
+    gitHTTP('GET', items[i].contributors_url + '?', function(err, response, contributors){
+      if(err){
+        console.log("Error in userController GIT request", err);
+      }
+    //console.log(typeof contributors);
+    contributors = JSON.parse(contributors);
+      contributors.forEach(function(element, index){
+        if (userObj[element.login]){
+          userObj[element.login].count++;
+          userObj[element.login].contributions += element.contributions;
+        }
+        else{
+          userObj[element.login] = {
+            name : element.login,
+            id: element.id,
+            url: element.url,
+            count: 1,
+            contributions: element.contributions
+          }
+        }
+      });
 
-  res.send(JSON.parse(body).items);
+      var arr = Object.keys(userObj).map(function (key) {return userObj[key]});
+      console.log(arr);
+
+    });
+  }
+
+  res.send(items);
 
 };
