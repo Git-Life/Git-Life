d14635adaedfd07f849cfffb70c21319927c26ef
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-02 15:04:22 -0700

    core: refactored searchcontroller to use secret

--- server/search/SearchController.js
+++ server/search/SearchController.js
@@ -1,7 +1,20 @@
 var path = require('path');
 var request = require('request');
+var secret = require('../splash/tempsecret.js');
+
 
 var root = 'https://api.github.com/';
+
+var secretURL = '&client_id=' + secret.id + '&client_secret=' + secret.secret;
+
+
+function gitHTTP(method, reqString, cb){
+  request({
+    uri: reqString + secretURL,
+    method: method,
+    headers: {'user-agent': 'node.js'}
+  }, cb);
+}
 
 module.exports = {
 	getRepos: function (req, res) {
@@ -14,6 +27,7 @@ module.exports = {
 			if(error){
         console.log('Error: ', error);
       }
+      console.log(body);
 			res.send(JSON.parse(body).items);
 		});
 	},
