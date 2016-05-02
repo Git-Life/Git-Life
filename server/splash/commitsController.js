ab4c67735142ae95faecbf5fbace72c7163ad6fe
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-02 11:30:59 -0700

    bug: pointed writefile function at existent folder

--- server/splash/commitsController.js
+++ server/splash/commitsController.js
@@ -1,11 +1,10 @@
 var request = require('request');
 var fs = require('fs');
 
-const SECRET = require('../../config.js');
+const SECRET = require('../config.js');
 const GITHUB_ROOT = 'https://api.github.com/';
 
 const SECRET_URL  = '&client_id=' + SECRET.clientID + '&client_secret=' + SECRET.clientSecret;
-console.log('in the commitsController file..');
 
 
 
@@ -19,12 +18,11 @@ module.exports = function(req, res){
       method: 'GET',
       headers: {'user-agent': 'node.js'}
       }, function (error, response, body) {
-        console.log('in function body (commitsController.js)');
           if(error){
             console.log('Error: ', error);
           }
 
-          fs.writeFile(__dirname + '/../dummyData_SH_temp/commits.txt', JSON.stringify(body), (err) => {
+          fs.writeFile(__dirname + '/../storage/commits.txt', JSON.stringify(body), (err) => {
             if(err){
               console.log('fs.writeFile error in server/splash/commitsController.js', err);
             }
