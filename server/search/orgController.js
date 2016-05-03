b980ae98d89ed12cf573a6f8501f992a050db2b2
Alex Paczynski <apaczy@gmail.com>
2016-05-03 15:18:51 -0700

    core: implemented server-side logic for parsing org search results

--- server/search/orgController.js
+++ server/search/orgController.js
@@ -1,16 +1,24 @@
-var secret  = require('./tempsecret');
+var secret  = require('../splash/tempsecret');
 var request = require('request');
 var async   = require('async');
 
 module.exports = {
+  getOrgs: function (resObj) {
 
-  getOrgs: function (req, res) {
+    resObj['organizations'] = [];
 
-    
-    // var query = req.query.searchTerm;
-    // request({
-    //   uri: root + 'search/'
-    // })
+    resObj.items.forEach(function(element, index){
+      if(element.owner.type === 'Organization'){
+        resObj['organizations'].push({
+          name: element.owner.login,
+          avatar_url: element.owner.avatar_url,
+          url: element.owner.html_url,
+          id: element.owner.id
+        });
+      }
+    });
+
+    console.log('organizations: ', resObj.organizations);
+    res.send(resObj);
   }
-  
-}
+};
