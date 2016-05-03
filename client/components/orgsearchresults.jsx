1fea17e38b13b9232d03d3e9ae74ba0174b22207
Alex Paczynski <apaczy@gmail.com>
2016-05-03 13:51:06 -0700

    core: created client-sode components for org search results

---
+++ client/components/orgsearchresults.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import OrgItem from './orgitem';
+
+export default class OrgSearchResults extends Component {
+
+  populateResults(){
+    if(this.props.results.data){
+      return _.reduce(this.props.results.data.items, (accum, item)=>{
+        let html =(
+            <OrgItem description={item.description} url={item.url} key={item.id}/>
+        );
+        accum.push(html);
+        return accum;
+      }, []);
+    }
+    else{
+    
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Org Search Results!
+        {this.populateResults()}
+      </div>
+    );
+  }
+
+};
