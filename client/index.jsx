74993c92ef7f9d989a38d8fa78c3e7855b9a544b
Jenna Gain <gain.jenna@gmail.com>
2016-05-03 11:17:34 -0700

    Core: Added App to Index

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/app';
 import App2 from './containers/app2';
 import RepoSecondPage from './containers/repo_secondpage';
 import configureStore from './store/configureStore';
@@ -11,7 +12,7 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App2/>
+    <App/>
   </Provider>,
     document.getElementById('app')
 );
