ec8919da587b60d4eab6ebd2591ce5fd1d3e833f
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-03 14:33:25 -0700

    wip: filtering user results

--- client/containers/userResults.jsx
+++ client/containers/userResults.jsx
@@ -2,33 +2,58 @@ import React, {Component} from 'react';
 import UserResult from '../components/userResult';
 
 export default class UserResults extends Component {
-  
-  getInitialState: function() {
-    return {results: this.props.initialResults};
-  },
 
 
-  populateResults(){
-    if(this.props.results.data){
-      return _.reduce(this.props.results.data.contributors, (accum, item)=>{
-        let html =(
-          <UserResult
-            name={item.name}
-            url={item.url}
-            key={item.id}
-            contributions={item.contributions}
-            count={item.count}
-            />
-        );
-        accum.push(html);
-        return accum;
-      }, []);
+  constructor(props){
+    super(props)
+  }
+
+  constructHTML(dataObj){
+
+    return _.reduce(dataObj, (accum, item)=>{
+      let html =(
+        <UserResult
+          name={item.name}
+          url={item.url}
+          key={item.id}
+          contributions={item.contributions}
+          count={item.count}
+          />
+      );
+      accum.push(html);
+      return accum;
+    }, []);
+
+  }
+
+  populateResults(sortBy){
+
+    switch (sortBy) {
+      case 'name':
+        constructHTML(this.props.results.data.contributors
+          .sort((a, b) => {
+            a.name - b.name;
+          }));
+        break;
+      case 'contributions':
+        constructHTML(this.props.results.data.contributors
+          .sort((a, b) => {
+            b.contributions - a.contributions;
+          }));
+        break;
+      case 'count':
+        constructHTML(this.props.results.data.contributors
+          .sort((a, b) => {
+            b.count - a.count;
+          }));
+        break;
+      default:
+        constructHTML(this.props.results.data.contributors
+          .sort((a, b) => {
+            b.count - a.count;
+          }));
+        break;
     }
-    else{
-
-    }
-
-
 
   }
 
