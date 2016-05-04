7f54c390a80146ca898eb1aadc242225da712914
Jenna Gain <gain.jenna@gmail.com>
2016-05-04 12:27:06 -0700

    Core: deleted line referring to old file

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RepoSecondPage from './containers/repo_secondpage';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {Router, hashHistory} from 'react-router';
