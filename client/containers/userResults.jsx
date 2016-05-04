4700e88e1c5c32eb6334ce06cafde7a6c70555fb
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-03 19:47:25 -0700

    reverted

--- client/containers/userResults.jsx
+++ client/containers/userResults.jsx
@@ -5,15 +5,11 @@ export default class UserResults extends Component {
 
 
   constructor(props){
-    super(props);
-    this.state = {filter: '', results: props.results};
-    this.constructHTML = this.constructHTML.bind(this);
-    this.handleFilter = this.handleFilter.bind(this);
-    this.populateResults = this.populateResults.bind(this);
-
+    super(props)
   }
 
   constructHTML(dataObj){
+
     return _.reduce(dataObj, (accum, item)=>{
       let html =(
         <UserResult
@@ -27,61 +23,48 @@ export default class UserResults extends Component {
       accum.push(html);
       return accum;
     }, []);
-  }
 
-  handleFilter(){
-
-    this.setState({filter: 'name'});
   }
 
   populateResults(sortBy){
-    if(this.state.results.data){
+    if(this.props.results.data){
       switch (sortBy) {
         case 'name':
-        console.log('this.state.results', this.state.results);
-          return this.constructHTML(this.state.results.data.contributors
+          return this.constructHTML(this.props.results.data.contributors
             .sort((a, b) => {
               return a.name - b.name;
             }));
           break;
         case 'contributions':
-          return this.constructHTML(this.state.results.data.contributors
+          return this.constructHTML(this.props.results.data.contributors
             .sort((a, b) => {
               return b.contributions - a.contributions;
             }));
           break;
         case 'count':
-          return this.constructHTML(this.state.results.data.contributors
+          return this.constructHTML(this.props.results.data.contributors
             .sort((a, b) => {
               return b.count - a.count;
             }));
           break;
         default:
-          return this.constructHTML(this.state.results.data.contributors
+          return this.constructHTML(this.props.results.data.contributors
             .sort((a, b) => {
               return b.count - a.count;
             }));
           break;
       }
     }
+
+
   }
 
   render() {
-    const results = this.populateResults(this.state.filter);
     return (
       <div className='collection'>
-
-
-        <button onClick={this.handleFilter}>Name</button>
-        <button onClick={this.handleFilter}>Contributions</button>
-
-
-        { results }
+        {this.populateResults('contributions')}
       </div>
     );
   }
 
 };
-
-//UserResults.propTypes = { results: React.PropTypes.object, filter: React.PropTypes.string };
-UserResults.defaultProps = { results: {}, filter: "name"};
