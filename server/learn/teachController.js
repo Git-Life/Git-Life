fd024a5f3b125004cbb95d839f33ae297b5b51dc
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-04 12:19:57 -0700

    core: successfully authenticated, gitignored auth file

--- server/learn/teachController.js
+++ server/learn/teachController.js
@@ -1,3 +1,18 @@
+var config = {
+  keyFilename: '../../gitalytics-5f17b3312e53.json',
+  projectId: 'gitalytics-1288'
+}
+var gcloud = require('gcloud')(config);
+var prediction = gcloud.prediction();
+prediction.createModel('gitTest', function(err, model, apiResponse){
+  if(err){
+    console.log("error in prediction", err);
+  }
+});
+
 module.exports = function(req, res){
   res.send('ok');
-};
+
+  };
+
+//
