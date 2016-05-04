72d365533703a6f05eadd09cd3bcd144c9253815
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-04 15:37:42 -0700

    core: added root level function to select a certain repo

--- client/components/searchitem.jsx
+++ client/components/searchitem.jsx
@@ -4,7 +4,11 @@ export default class SearchItem extends Component{
   render(){
     return (
 
-      <div className='collection-item' class="badge">
+      <div
+        className='collection-item'
+        class="badge"
+        onClick={()=>{this.props.selectRepoIssues(this.props.issuesUrl)}}
+        >
         <div><a href={this.props.repoUrl} >{this.props.description}</a><a style={{float: 'right'}}>{this.props.issues}</a></div>
 
       </div>
