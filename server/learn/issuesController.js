fda8c908cc7260dbc65c9b13dc9f38fb7165db42
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-04 19:08:36 -0700

    core: built out basic serverside logic for sending back issues array

--- server/learn/issuesController.js
+++ server/learn/issuesController.js
@@ -1,4 +1,23 @@
+var request = require('request');
+var secret = require('../splash/tempsecret.js');
+
+var root = 'https://api.github.com/';
+
+var secretURL = '&client_id=' + secret.id + '&client_secret=' + secret.secret;
+
+function gitHTTP(method, reqString, cb){
+  request({
+    uri: reqString + secretURL,
+    method: method,
+    headers: {'user-agent': 'node.js'}
+  }, cb);
+}
+
+
 module.exports = function(req, res){
   var issuesURL = req.query.issuesURL;
-
+  //url.slice(0, url.length-9)
+  gitHTTP('GET', issuesURL.slice(0, issuesURL.length-9), (err, response, body)=>{
+    res.send(body);
+  });
 }
