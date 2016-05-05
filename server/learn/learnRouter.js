b4b1835da93ce2364fa61809a4b46e16f2618e5f
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-04 18:59:06 -0700

    core: added issues router to learn router

--- server/learn/learnRouter.js
+++ server/learn/learnRouter.js
@@ -1,5 +1,7 @@
 teachController = require('./teachController.js');
+issuesController = require('./issuesController.js');
 
 module.exports = function(app){
   app.get('/teach', teachController);
+  app.get('/issues', issuesController);
 }
