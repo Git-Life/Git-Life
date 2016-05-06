f627dd74e3d10d79e1f37cca76dc3af65e5d6222
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-06 15:08:29 -0700

    core: added axios and built single post request

--- client/components/issueitem.jsx
+++ client/components/issueitem.jsx
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
+import Axios from 'axios';
+
 export default class IssueItem extends Component{
   sendTeach(difficulty){
     console.log(difficulty);
+    axios.post('/teach',{
+      issue: this.props.issue,
+      difficulty: difficulty
+    })
+    .then((res) => {
+      console.log('this was res', res);
+    })
+    .catch((res)=> {
+      console.log('caught this res', res);
+    })
   }
 
   render(){
