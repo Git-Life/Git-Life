677c8525e3066664726b8fd0bb4d2b9635b66e98
Alex Paczynski <apaczy@gmail.com>
2016-05-06 16:33:32 -0700

    core: built out server-side routing and GitHub request for new repos

--- server/splash/newReposController.js
+++ server/splash/newReposController.js
@@ -5,20 +5,23 @@ module.exports = {
 
   getNewRepos: function (req, res) {
 
-    var repos = {};
-    var root = 'https://api.github.com/';
-    var gitRequest = 'search/repositories?q=size:>80&created>2016-04-01&sort=stars&order=desc'; // &per_page=100
-    var auth = '&client_id=' + secret.id + '&client_secret=' + secret.secret;
-
     var today = new Date();
     console.log('today: ', today.toLocaleDateString());
 
     //var lastMonth = new Date(today.setUTCMonth(today.getUTCMonth() - 1));
     //console.log('today minus one month: ', lastMonth.toLocaleDateString());
 
-
     var lastWeek = new Date(today.setUTCHours(today.getUTCHours() - (8 * 24)));
-    console.log('today minus one week: ', lastWeek.toLocaleDateString());
+    console.log('today minus one week: ', lastWeek.toLocaleDateString()); // 3/29/2016
+
+    var lastWeekArr = lastWeek.toLocaleDateString().split('/');
+    var lastWeekString = lastWeekArr[2] + '-' + lastWeekArr[0] + '-' + lastWeekArr[1];
+    console.log('last week: ', lastWeekString);
+
+    var repos = {};
+    var root = 'https://api.github.com/';
+    var gitRequest = 'search/repositories?q=size:>80&created>' + lastWeekString + '&sort=stars&order=desc'; // &per_page=100 // 2016-04-01
+    var auth = '&client_id=' + secret.id + '&client_secret=' + secret.secret;
 
     // var findOrgs = function (repos){
     //   for(var i = 0; i < repos.length; i++){
@@ -43,9 +46,9 @@ module.exports = {
           console.log('Error: ', error);
         }
         console.log(JSON.parse(body).items);
-        findOrgs(JSON.parse(body).items);
+        //findOrgs(JSON.parse(body).items);
 
-        res.send(orgs);
+        res.send(JSON.parse(body).items);
       });
     };
 
