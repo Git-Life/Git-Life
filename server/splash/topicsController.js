01bc1b77160ad38cf9ab3c2e9e605d529ef193d0
Jenna Gain <gain.jenna@gmail.com>
2016-05-07 16:57:24 -0700

    Bug: working on search bug

--- server/splash/topicsController.js
+++ server/splash/topicsController.js
@@ -87,4 +87,30 @@ module.exports={
     });
 
   }
+
+  getGoogleFeed: function(req, res){
+    var req = request('https://news.google.com/news?q=' + lemurs + '&output=rss'),
+    feedparser = new FeedParser();
+    req.on('error', function (error) {
+    });
+    req.on('response', function (res) {
+      var stream = this;
+      if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+      stream.pipe(feedparser);
+    });
+    feedparser.on('error', function(error) {
+    });
+      var results = [];
+
+    feedparser.on('readable', function() {
+      var stream = this
+      var meta = this.meta
+      while (item = stream.read()) {
+        results.push(item);
+      }
+    });
+
+    feedparser.on('end', ()=>{
+      res.send(results);
+    });
 }
