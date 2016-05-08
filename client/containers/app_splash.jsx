74e4dbffb0f90cbf6c79302c144a68bf0e93444f
Alex Paczynski <apaczy@gmail.com>
2016-05-07 17:26:15 -0700

    core: removed TrendingNewOrgs child from app_splash

--- client/containers/app_splash.jsx
+++ client/containers/app_splash.jsx
@@ -23,7 +23,7 @@ class AppSplash extends Component {
           <SplashRepos getSplashRepos={this.props.actions.getSplashRepos} repos={this.props.repos}/>
           <WiredResults hnresults={this.props.hnResults} searchHN={this.props.actions.searchHN} searchData={this.props.actions.searchData} dataResults={this.props.dataResults} wired={this.props.actions.searchWired} wiredResults={this.props.wiredResults}/>
           <TrendingNewRepos getNewRepos={this.props.actions.getNewRepos} newRepos={this.props.newRepos} />
-          <TrendingNewOrgs getNewOrgs={this.props.actions.getNewOrgs} newOrgs={this.props.newOrgs} />
+
         </div>
     </div>
     );
