41924770b0be737d1bcaccc6b37047af01ee75a3
Jenna Gain <gain.jenna@gmail.com>
2016-05-09 17:31:39 -0700

    Style: Added minor styling

--- client/components/repoview.jsx
+++ client/components/repoview.jsx
@@ -34,23 +34,23 @@ export default class SelectedRepo extends Component{
 
   render(){
     return (
+
       <div class='modal-content'>
         <h4>{this.state.repo.name}</h4>
         <h5>{this.state.repo.description}</h5>
         <div>Forks: {this.state.repo.forks} | Issues: {this.state.repo.open_issues} | Watched By: {this.state.repo.watchers} | Stars: {this.state.repo.stargazers_count}</div>
 
-      <button
-          className='waves-effect waves-light btn'
-          onClick={()=>{this.getIssues()}}>
-          Get Issues
+        <button
+            className='waves-effect waves-light btn'
+            onClick={()=>{this.getIssues()}}>
+            Get Issues
         </button>
 
         <ul className='collection'>
-      
+
         <button onClick={()=>{this.getIssues()}}>Get Issues</button>
         {this.state.repo.name}
         <ul >
-
           {this.populateResults()}
         </ul>
       </div>
