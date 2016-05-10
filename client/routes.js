ca9fdcb78b6731e0c2b7efbf7b17036cfdb37376
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-10 16:02:38 -0700

    core: tabs switch between trends and searchresults, empty components display without search still

--- client/routes.js
+++ client/routes.js
@@ -4,12 +4,11 @@ import {Route, IndexRoute} from 'react-router';
 import App from './containers/app';
 import AppSearch from './containers/app_search';
 import AppSplash from './containers/app_splash';
-import SearchBar from './containers/searchbar';
 
 export default(
   <Route>
     <Route path="/" component={App} >
-      <IndexRoute component={SearchBar} />
+      <IndexRoute component={AppSearch} />
       <Route path="trends" component={AppSplash}/>
       <Route path="search" component={AppSearch}/>
     </Route>
