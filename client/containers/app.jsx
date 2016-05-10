8ba043d8863af5c0fd8430b9c0abd4879ea67bce
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-10 14:56:55 -0700

    core: materialize javascript functions appear to be working

--- client/containers/app.jsx
+++ client/containers/app.jsx
@@ -19,7 +19,7 @@ class App extends Component {
             <div className="row">
               <div className="col s12">
                 <ul className='tabs'>
-                  <li className='tab col s3'><a href="#test1">Test 1</a></li>
+                  <li className='tab col s3'><a href="#test1">Test 1-</a></li>
                   <li className='tab col s3'><a class="active" href="#test2">Test 2</a></li>
                   <li className='tab col s3'><a href="#test4">Test 4</a></li>
                 </ul>
