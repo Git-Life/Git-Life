cfce3eaeb46e5d17c145692f14419e1624388da0
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-12 16:15:23 -0700

    syn: improved orgitem readability

--- client/components/orgitem.jsx
+++ client/components/orgitem.jsx
@@ -5,7 +5,15 @@ export default class OrgItem extends Component{
     return (
       <li className='collection-item avatar valign-wrapper' >
         <img src={this.props.avatar_url} alt='org avatar' className='circle valign'/>
-        <span className='title valign right-align'><a className='title valign right-align' href={this.props.url} target='_blank'>{this.props.description}</a></span>
+        <span
+          className='title valign right-align'>
+          <a
+            className='title valign right-align'
+            href={this.props.url}
+            target='_blank'>
+              {this.props.description}
+          </a>
+        </span>
       </li>
     );
   }
