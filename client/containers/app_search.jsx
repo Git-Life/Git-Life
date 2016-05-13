260faab2efb25789b0216e72f49dfd8198861c33
Caleb Rogers <rogersjcaleb@gmail.com>
2016-05-12 17:00:36 -0700

    core: removed zfactor and standardized color to use interal columns as seperators

--- client/containers/app_search.jsx
+++ client/containers/app_search.jsx
@@ -39,20 +39,16 @@ class AppSearch extends Component {
 
         <div className='section row'>
         {this.state.showResults ?
-          <div style={{padding: "5px"}}><div className='col s4 blue-grey lighten-4 z-depth-2'>
-            <UserResults  results={this.props.results} /> </div>
-          </div> : null}
+          <div className='col s4 blue-grey lighten-5'><UserResults  results={this.props.results} /> </div>  : null}
         {this.state.showResults ?
-          <div style={{padding: "5px"}}><div className='col s4  blue-grey lighten-4 z-depth-2' > <RepoSearchResults
+          <div className='col s4  blue-grey lighten-5' > <RepoSearchResults
             results={this.props.results}
             selectRepo={this.props.actions.selectRepo}
             selectedRepo={this.props.selectedRepo}
             issues={this.props.issues}
-            getIssues={this.props.actions.getIssues}/> </div></div> : null}
+            getIssues={this.props.actions.getIssues}/> </div> : null}
         {this.state.showResults ?
-          <div style={{padding: "5px"}}><div className='col s4  blue-grey lighten-4 z-depth-2'>
-            <OrgSearchResults results={this.props.results} />
-          </div> </div> : null}
+          <div className='col s4  blue-grey lighten-5'> <OrgSearchResults results={this.props.results} /></div> : null}
         </div>
 
     </div>
