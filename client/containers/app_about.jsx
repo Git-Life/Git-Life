c822e33d6f7df5a4b42213c0520e153b45e31685
Alex Paczynski <apaczy@gmail.com>
2016-05-15 17:40:46 -0700

    core: updated About page layout and styles

--- client/containers/app_about.jsx
+++ client/containers/app_about.jsx
@@ -12,14 +12,13 @@ export default class AppAbout extends Component {
           <h4>Mission</h4>
           <p>
             Gitalytics is designed to help you quickly find the projects you care about.
-            <br />
             Using the GitHub Search API, Gitalytics optimizes the search for interesting repos,
             users, and organizations. The top new and overall trending repos and organizations
             can also be found on the trends page.
           </p>
           <h4>Tech Stack</h4>
           <p>
-            The tools used to build this app
+            Tools used to build this app
           </p>
           <h4>Dev Team</h4>
           <p>
@@ -28,24 +27,42 @@ export default class AppAbout extends Component {
           <div className='row'>
             <div className='col s12 m12 l12'>
               <div className='teamMember col s4 m4 l4'>
-                <div>Jenna Gain</div>
+                <div className='name'>Jenna Gain</div>
                 <img className='responsive-img circle imgDev' src='./images/jenna.jpeg' />
-                <div>
-                  <a href='https://github.com/gainjenna' >GitHub</a>
+                <div className='info'>
+                  { ' ' }
+                  <br />
+                  Software Engineer
+                  <br />
+                  <a href='https://github.com/gainjenna' target='_blank'>
+                    GitHub
+                  </a>
                 </div>
               </div>
               <div className='teamMember col s4 m4 l4'>
-                <div>Alex Paczynski</div>
+                <div className='name'>Alex Paczynski</div>
                 <img className='responsive-img circle imgDev' src='./images/alex.jpg' />
-                <div>
-                  <a href='https://github.com/re1nv3nt' >GitHub</a>
+                <div className='info'>
+                  Product Owner
+                  <br />
+                  Software Engineer
+                  <br />
+                  <a href='https://github.com/re1nv3nt' target='_blank'>
+                    GitHub
+                  </a>
                 </div>
               </div>
               <div className='teamMember col s4 m4 l4'>
-                <div>Caleb Rogers</div>
+                <div className='name'>Caleb Rogers</div>
                 <img className='responsive-img circle imgDev' src='./images/caleb.jpeg' />
-                <div>
-                  <a href='https://github.com/komali2' >GitHub</a>
+                <div className='info'>
+                  Scrum Master
+                  <br />
+                  Software Engineer
+                  <br />
+                  <a href='https://github.com/komali2' target='_blank'>
+                    GitHub
+                  </a>
                 </div>
               </div>
             </div>
